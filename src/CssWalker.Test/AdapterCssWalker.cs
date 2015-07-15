cc5c937bf787ebfa7ceeecdfb9a8c60a198010d2
gBritz <gbritzv@gmail.com>
2015-07-15 13:48:05 -0300

    Test Project with two tests

---
+++ src/CssWalker.Test/AdapterCssWalker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CssWalker.Test
+{
+    public class AdapterCssWalker : CssWalker
+    {
+        public Action<String, Int32, Int32> OnBeginSelector { get; set; }
+
+        public Action<String, Int32, Int32> OnEndSelector { get; set; }
+
+        protected override void VisitBeginSelector(string selector, int line, int column)
+        {
+            if (OnBeginSelector != null)
+                OnBeginSelector(selector, line, column);
+        }
+
+        protected override void VisitEndSelector(string selector, int line, int column)
+        {
+            if (OnEndSelector != null)
+                OnEndSelector(selector, line, column);
+        }
+    }
+}
---
+++ src/CssWalker.Test/CssWalkerTest.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace CssWalker.Test
+{
+    [TestClass]
+    public class CssWalkerTest
+    {
+        [TestMethod]
+        public void GivenOneEmptyCssClassButton_ClassNameShouldBeButton()
+        {
+            var reader = new StringReader(@" .button {}");
+
+            var walker = new AdapterCssWalker
+            {
+                OnBeginSelector = (selector, line, column) => selector.Should().Be(".button ")
+            };
+
+            walker.Visit(reader);
+        }
+
+        [TestMethod]
+        public void GivenOneCssClassButtonWithTwoProperties_ClassNameShouldBeButton()
+        {
+            var reader = new StringReader(@"
+                .button {
+                    margin-top: 10px;
+                    background-image: url(../img/bk.jpg);
+                }
+            ");
+
+            var walker = new AdapterCssWalker
+            {
+                OnBeginSelector = (selector, line, column) => selector.Should().Be(".button ")
+            };
+
+            walker.Visit(reader);
+        }
+    }
+}
